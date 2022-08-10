e937dc6fe3411402789f9fe3539776d368ebec11
A S <wickedgamblah@gmail.com>
2022-08-10 08:44:28 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -61,6 +61,7 @@ class Dev(Configuration):
         "allauth.socialaccount",
         "allauth.socialaccount.providers.google",
         "rest_framework",
+        "rest_framework.authtoken",
     ]
 
     MIDDLEWARE = [
@@ -204,11 +205,14 @@ class Dev(Configuration):
     ACCOUNT_USERNAME_REQUIRED = False
     ACCOUNT_AUTHENTICATION_METHOD = "email"
 
-    
-
-
-
-   
+    REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
+ 
 
 class Prod(Dev):
     DEBUG = False
--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.authtoken import views
+
 
 from blog.api.views import PostList, PostDetail
+
 
 urlpatterns = [
     path("posts/", PostList.as_view(), name="api_post_list"),
@@ -9,3 +13,10 @@ urlpatterns = [
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns += [
+    path("auth/", include("rest_framework.urls")),
+    path("token-auth/", views.obtain_auth_token)
+]
+
+
