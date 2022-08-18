c572dcfecc8584ef3d0e3ed344ca4ce08b9b79a9
A S <wickedgamblah@gmail.com>
2022-08-18 09:16:21 +0000

    Finish SimpleJWT

--- blango/settings.py
+++ blango/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from configurations import Configuration
 from configurations import values
 import dj_database_url
+from datetime import timedelta
+
 
 
 class Dev(Configuration):
@@ -208,6 +210,13 @@ class Dev(Configuration):
     ACCOUNT_AUTHENTICATION_METHOD = "email"
 
     REST_FRAMEWORK = {
+        "DEFAULT_AUTHENTICATION_CLASSES": [
+            "rest_framework.authentication.BasicAuthentication",
+            "rest_framework.authentication.SessionAuthentication",
+            "rest_framework.authentication.TokenAuthentication",
+            "rest_framework_simplejwt.authentication.JWTAuthentication"
+        ],
+
         "DEFAULT_THROTTLE_CLASSES": [
             "blog.api.throttling.AnonSustainedThrottle",
             "blog.api.throttling.AnonBurstThrottle",
@@ -221,12 +230,6 @@ class Dev(Configuration):
             "user_sustained": "5000/day",
             "user_burst": "100/minute",
         },
-
-        "DEFAULT_AUTHENTICATION_CLASSES": [
-            "rest_framework.authentication.BasicAuthentication",
-            "rest_framework.authentication.SessionAuthentication",
-            "rest_framework.authentication.TokenAuthentication",
-        ],
         
         "DEFAULT_PERMISSION_CLASSES": [
             "rest_framework.permissions.IsAuthenticatedOrReadOnly"
@@ -240,6 +243,11 @@ class Dev(Configuration):
             "rest_framework.filters.OrderingFilter",
         ],
     }   
+    
+    SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    }
 
     SWAGGER_SETTINGS = {
         "SECURITY_DEFINITIONS": {
--- blog/api/urls.py
+++ blog/api/urls.py
@@ -9,6 +9,9 @@ from rest_framework.routers import DefaultRouter
 
 from blog.api.views import UserDetail, TagViewSet, PostViewSet 
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
 
 schema_view = get_schema_view(
     openapi.Info(title="Blango API", default_version="v1", description="API for Blango",),
@@ -31,6 +34,8 @@ router.register("posts", PostViewSet)
 urlpatterns += [
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token),
+    path("jwt/", TokenObtainPairView.as_view(), name="jwt_obtain_pair"),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt_refresh"),
     re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0),
         name="schema-json",),
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0),
